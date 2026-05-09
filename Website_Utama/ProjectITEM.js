36e0b252810ff88b6b9ec3d6812fac95ed7c0989
Ahmadrijani <samsuljani174@gmail.com>
2026-05-09 14:53:09 +0700

    Create ProjectITEM.js

---
+++ Website_Utama/ProjectITEM.js
@@ -0,0 +1,179 @@
+const ProjectITEM = {
+  projek: [
+    {
+      kategori: "Prestasi",
+      items: [
+        {
+          judul: "JUARA 1 LOMBA DESAIN POSTER HARI KARTINI - AHMAD RIJANI - SMKN 2 TAMIANG LAYANG",
+          tag: "Prestasi",
+          url: "https://youtu.be/r17SrXKl1fM?si=GQYkrDVPmY1766jc",
+          thumbnail: "https://img.youtube.com/vi/r17SrXKl1fM/hqdefault.jpg",
+          tipe: "video"
+        },
+        {
+          judul: "LOMBA POSTER HARI KARTINI - AHMAD RIJANI - SMKN 2 TAMIANG LAYANG",
+          tag: "Prestasi",
+          url: "https://youtu.be/ooS5hSy_8cA?si=YertOd05sTeWNeGF",
+          thumbnail: "https://img.youtube.com/vi/ooS5hSy_8cA/hqdefault.jpg",
+          tipe: "video"
+        },
+        {
+          judul: "Proses Pembuatan Logo dalam Seleksi LKSN Desain – Ahmad Rijani (i'm win)– SMKN 2 Tamiang Layang",
+          tag: "Prestasi",
+          url: "https://youtu.be/zLDWnw8X5Jo?si=yKD5EAcC6RWYcZZ_",
+          thumbnail: "https://img.youtube.com/vi/zLDWnw8X5Jo/hqdefault.jpg",
+          tipe: "video"
+        }
+      ]
+    },
+    {
+      kategori: "Project Gelas",
+      items: [
+        {
+          judul: "Project Gelas: Dokumentasi Lengkap Proses Desain",
+          tag: "Series Part 1-8",
+          url: "https://youtube.com/playlist?list=PL40aAKe3J5ED_tQJMVq16Ur0B3oUUpIeF",
+          thumbnail: "https://img.youtube.com/vi/ALI7pQon7MY/mqdefault.jpg",
+          tipe: "playlist"
+        },
+        {
+          judul: "Proses Edit Desain Gelas Secara Langsung",
+          tag: "Live Edit",
+          url: "https://www.youtube.com/live/r2ztxbuQKaQ",
+          thumbnail: "https://img.youtube.com/vi/r2ztxbuQKaQ/mqdefault.jpg",
+          tipe: "video"
+        },
+        {
+          judul: "Gelas",
+          tag: "Playlist",
+          url: "https://youtube.com/playlist?list=PL40aAKe3J5EC1xCo_n-jzZh3l66iBqH3u&si=ZdiFBjWEVY0gIpuB",
+          thumbnail: "https://i.ytimg.com/pl_c/PL40aAKe3J5EC1xCo_n-jzZh3l66iBqH3u/studio_square_thumbnail.jpg?sqp=CPKM-88G-oaymwEICKoDEPABSFqi85f_AwYI3rH8yAY=&rs=AOn4CLCK3B5vA-rkimrvppNyKANtHIs--g",
+          tipe: "playlist"
+        }
+      ]
+    },
+    {
+      kategori: "Cinema & Film Project",
+      items: [
+        {
+          judul: "Project Film Pendek 1",
+          tag: "Film 1",
+          url: "https://youtu.be/TpZ31rFJ0eI",
+          thumbnail: "https://img.youtube.com/vi/TpZ31rFJ0eI/mqdefault.jpg",
+          tipe: "video"
+        },
+        {
+          judul: "Project Film Pendek 2 (Original)",
+          tag: "Film 2",
+          url: "https://youtu.be/TpZ31rFJ0eI",
+          thumbnail: "https://img.youtube.com/vi/_d-m8Bmivko/mqdefault.jpg",
+          tipe: "video"
+        },
+        {
+          judul: "Film 2 Remake: Good Ending Version",
+          tag: "Good Ending",
+          url: "https://youtu.be/znLPJykkFtI",
+          thumbnail: "https://img.youtube.com/vi/znLPJykkFtI/mqdefault.jpg",
+          tipe: "video"
+        },
+        {
+          judul: "Film 2: Bad Ending Version",
+          tag: "Bad Ending",
+          url: "https://youtu.be/d_jL3BCCsHk",
+          thumbnail: "https://img.youtube.com/vi/d_jL3BCCsHk/mqdefault.jpg",
+          tipe: "video"
+        },
+        {
+          judul: "Pembuatan Properti Film:Sabu impact  (Bahaya Sabu)",
+          tag: "Film",
+          url: "https://youtu.be/iRbleP4RgCA?si=ktxJqBV1EXXaP2uC",
+          thumbnail: "https://img.youtube.com/vi/iRbleP4RgCA/hqdefault.jpg",
+          tipe: "video"
+        },
+        {
+          judul: "IDE DARI FILM Sabu Impact (Bahaya Sabu)",
+          tag: "Film",
+          url: "https://youtu.be/TJoeUTRDWAI?si=haIUCEZPneW_c9bn",
+          thumbnail: "https://img.youtube.com/vi/TJoeUTRDWAI/hqdefault.jpg",
+          tipe: "video"
+        }
+      ]
+    },
+    {
+      kategori: "IT & Networking",
+      items: [
+        {
+          judul: "Tugas Konfigurasi Debian Server",
+          tag: "Debian",
+          url: "https://youtu.be/taUkb86I4Hg",
+          thumbnail: "https://img.youtube.com/vi/taUkb86I4Hg/mqdefault.jpg",
+          tipe: "video"
+        },
+        {
+          judul: "Cisco Packet Treacer",
+          tag: "Playlist",
+          url: "https://www.youtube.com/playlist?list=PL40aAKe3J5ECkhl5HE-c_uakV0Zbo0W5p",
+          thumbnail: "https://i.ytimg.com/an_webp/y2pZ4--XITA/mqdefault_6s.webp?du=3000&sqp=CKCs-88G&rs=AOn4CLBS4Lrm5h5jvvO90-jM7R-C0PHIrA",
+          tipe: "playlist"
+        },
+        {
+          judul: "Virtual Box",
+          tag: "Playlist",
+          url: "https://youtube.com/playlist?list=PL40aAKe3J5EBvZ0lLyA6P3QxDryILp_MZ&si=lw-J2uOCab2ElKYt",
+          thumbnail: "https://i.ytimg.com/vi/zhJc1H3rPJU/hqdefault.jpg?sqp=-oaymwEnCPYBEIoBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLAlQ0wUjlphL1VDFKwPJmH96mD6og",
+          tipe: "playlist"
+        }
+      ]
+    },
+    {
+      kategori: "Explore",
+      items: [
+        {
+          judul: "Explore: Dokumentasi Perjalanan",
+          tag: "Adventure",
+          url: "https://youtu.be/zwI8BwUTzsI",
+          thumbnail: "https://img.youtube.com/vi/zwI8BwUTzsI/mqdefault.jpg",
+          tipe: "video"
+        },
+        {
+          judul: "Dokumentasi",
+          tag: "Playlist",
+          url: "https://www.youtube.com/playlist?list=PL40aAKe3J5EARkFS2uFqmu4NV_PLYD-OH",
+          thumbnail: "https://i.ytimg.com/vi/FAmVXdbeTWo/hqdefault.jpg?sqp=-oaymwEnCPYBEIoBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLBPcXtj6WUoZe13hE42mpEaGQpyRw",
+          tipe: "playlist"
+        },
+        {
+          judul: "Tong Sampah",
+          tag: "Playlist",
+          url: "https://youtube.com/playlist?list=PL40aAKe3J5EBhuC2xNQXcBUsRel088LGl&si=2ljnDshgmnaNpVfe",
+          thumbnail: "https://i.ytimg.com/vi/t7cACAamlrg/hqdefault.jpg?sqp=-oaymwEnCPYBEIoBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDMILI_Uso_-uIILwW2GR3LZMwkNw",
+          tipe: "playlist"
+        }
+      ]
+    },
+    {
+      kategori: "School & Tutorial",
+      items: [
+        {
+          judul: "TUGAS SEKOLAH",
+          tag: "Playlist",
+          url: "https://www.youtube.com/playlist?list=PL40aAKe3J5ECogilj7wZksRDSjCbIoyHJ",
+          thumbnail: "https://i.ytimg.com/vi/yiLiCBAoDjU/hqdefault.jpg?sqp=-oaymwEnCPYBEIoBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDp7SMlUEmUuJCHcHTms34IAV4L9g",
+          tipe: "playlist"
+        },
+        {
+          judul: "Tutorial Blogger: Pasang Tombol Like di Postingan Kamu!",
+          tag: "Playlist",
+          url: "https://www.youtube.com/playlist?list=PL40aAKe3J5EBnLDwIXRCFIjM3FemjdJb4",
+          thumbnail: "https://i.ytimg.com/vi/Q-vUEVGOY14/hqdefault.jpg?sqp=-oaymwEnCPYBEIoBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDfG1bSsTR3kZ1kNSDJkYzmqD1jqA",
+          tipe: "playlist"
+        }
+      ]
+    }
+  ],
+  footer: {
+    judul: "Lihat lainnya di YouTube @ahmadrijaniofficial",
+    url: "https://www.youtube.com/@ahmadrijaniofficial",
+    thumbnail: "https://i.pinimg.com/736x/34/e4/e7/34e4e751e0dfbb4e3e222e33634d8cab.jpg"
+  }
+};
